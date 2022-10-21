cefdf0e400a8ecb25eaa207b86c61e753e7b1203
Cleverson Fernando <cfrodrigues50@gmail.com>
2022-10-21 16:14:04 -0300

    Projeto criado

---
+++ src/main/java/com/cleversonfernando/workshopspringboot2/Workshopspringboot2Application.java
@@ -0,0 +1,13 @@
+package com.cleversonfernando.workshopspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopspringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopspringboot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleversonfernando/workshopspringboot2/Workshopspringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleversonfernando.workshopspringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopspringboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
