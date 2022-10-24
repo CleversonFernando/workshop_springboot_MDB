a7adc0801ea0303780a9a5fcc2d5e21ffe5080b9
Cleverson Fernando <cfrodrigues50@gmail.com>
2022-10-24 15:52:39 -0300

    deleção de usuário com DELETE

--- src/main/java/com/cleversonfernando/workshopspringboot2/resources/UserResources.java
+++ src/main/java/com/cleversonfernando/workshopspringboot2/resources/UserResources.java
@@ -22,21 +22,29 @@ public class UserResources {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<UserDTO>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
         List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
+
     @PostMapping
     public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
         User obj = service.fromDTO(objDto);
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/cleversonfernando/workshopspringboot2/services/UserService.java
+++ src/main/java/com/cleversonfernando/workshopspringboot2/services/UserService.java
@@ -29,7 +29,12 @@ import java.util.Optional;
     public User insert(User obj) {
         return repo.insert(obj);
     }
+    public void delete(String id){
+        findById(id);
+        repo.deleteById(id);
+    }
         public User fromDTO(UserDTO objDto){
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
+
 }
