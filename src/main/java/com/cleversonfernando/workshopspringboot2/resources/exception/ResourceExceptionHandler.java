248e316bcda6b5fe899571236e8e98442a6a6675
Cleverson Fernando <cfrodrigues50@gmail.com>
2022-10-24 10:48:56 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/cleversonfernando/workshopspringboot2/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.cleversonfernando.workshopspringboot2.resources.exception;
+
+import com.cleversonfernando.workshopspringboot2.services.exception.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandartError err = new StandartError(System.currentTimeMillis(), status.value(), "Não encontrado!", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/cleversonfernando/workshopspringboot2/services/UserService.java
+++ src/main/java/com/cleversonfernando/workshopspringboot2/services/UserService.java
@@ -3,10 +3,12 @@ package com.cleversonfernando.workshopspringboot2.services;
 
 import com.cleversonfernando.workshopspringboot2.domain.User;
 import com.cleversonfernando.workshopspringboot2.repository.UserRepository;
+import com.cleversonfernando.workshopspringboot2.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
     public class UserService {
@@ -16,5 +18,10 @@ import java.util.List;
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 }
