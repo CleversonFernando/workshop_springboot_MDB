a39370632bd93d28de03d028dccb40fcb1c08ed9
Cleverson Fernando <cfrodrigues50@gmail.com>
2022-10-24 17:20:12 -0300

    Atualização de usuário com PUT

--- src/main/java/com/cleversonfernando/workshopspringboot2/resources/UserResources.java
+++ src/main/java/com/cleversonfernando/workshopspringboot2/resources/UserResources.java
@@ -47,4 +47,11 @@ public class UserResources {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
-}
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.noContent().build();
+}}
--- src/main/java/com/cleversonfernando/workshopspringboot2/services/UserService.java
+++ src/main/java/com/cleversonfernando/workshopspringboot2/services/UserService.java
@@ -33,6 +33,17 @@ import java.util.Optional;
         findById(id);
         repo.deleteById(id);
     }
+
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+    public void updateData(User newObj, User obj){
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
         public User fromDTO(UserDTO objDto){
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
